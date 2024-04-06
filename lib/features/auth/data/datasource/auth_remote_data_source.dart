7d6a545943a2a2dc63053194d9c53fc3e967d6d7
Luis Felipe Bardi <luisfbardi@gmail.com>
2024-04-06 18:25:09 -0300

    auth remote datasource and repository

---
+++ lib/features/auth/data/datasource/auth_remote_data_source.dart
@@ -0,0 +1,55 @@
+import 'package:blog_app/core/error/exceptions.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+abstract interface class AuthRemoteDataSource {
+  Future<String> signUpWithEmailAndPassword({
+    required String name,
+    required String email,
+    required String password,
+  });
+
+  Future<String> signInWithEmailAndPassword({
+    required String email,
+    required String password,
+  });
+}
+
+class AuthRemoteDataSourceImpl implements AuthRemoteDataSource {
+  final SupabaseClient supabaseClient;
+
+  AuthRemoteDataSourceImpl(this.supabaseClient);
+
+  @override
+  Future<String> signInWithEmailAndPassword({
+    required String email,
+    required String password,
+  }) async {
+    // TODO: implement signInWithEmailAndPassword
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<String> signUpWithEmailAndPassword({
+    required String name,
+    required String email,
+    required String password,
+  }) async {
+    try {
+      final response = await supabaseClient.auth.signUp(
+        email: email,
+        password: password,
+        data: {
+          'name': name,
+        },
+      );
+
+      if (response.user == null) {
+        throw const ServerException('User is null.');
+      }
+
+      return response.user!.id;
+    } catch (e) {
+      throw ServerException(e.toString());
+    }
+  }
+}
--- lib/features/auth/domain/repository/auth_repository.dart
+++ lib/features/auth/domain/repository/auth_repository.dart
@@ -1,6 +1,6 @@
 import 'package:fpdart/fpdart.dart';
 
-import '../../../../core/error/failure.dart';
+import '../../../../core/error/failures.dart';
 
 abstract interface class AuthRepository {
   Future<Either<Failure, String>> signUpWithEmailAndPassword({
