99eeaa17cc4cf38375d20fafbb052c95e2905f2e
Luis Felipe Bardi <luisfbardi@gmail.com>
2024-04-07 18:46:05 -0300

    init app user cubit dependency

--- lib/init_dependencies.dart
+++ lib/init_dependencies.dart
@@ -1,3 +1,4 @@
+import 'package:blog_app/core/common/cubits/app_user/app_user_cubit.dart';
 import 'package:blog_app/core/secrets/app_secrets.dart';
 import 'package:blog_app/features/auth/domain/repository/auth_repository.dart';
 import 'package:blog_app/features/auth/domain/usecases/user_sign_in.dart';
@@ -6,6 +7,7 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 
 import 'features/auth/data/datasource/auth_remote_data_source.dart';
 import 'features/auth/data/repository/auth_repository_impl.dart';
+import 'features/auth/domain/usecases/get_current_user.dart';
 import 'features/auth/domain/usecases/user_sign_up.dart';
 import 'features/auth/presentation/bloc/auth_bloc.dart';
 
@@ -19,6 +21,9 @@ Future<void> initDependencies() async {
   );
 
   serviceLocator.registerLazySingleton(() => supabase.client);
+
+  //core
+  serviceLocator.registerLazySingleton(() => AppUserCubit());
 }
 
 void _initAuth() {
@@ -34,7 +39,16 @@ void _initAuth() {
   serviceLocator.registerLazySingleton<UserSignIn>(
     () => UserSignIn(serviceLocator()),
   );
+  serviceLocator.registerLazySingleton<GetCurrentUser>(
+    () => GetCurrentUser(serviceLocator()),
+  );
+
   serviceLocator.registerLazySingleton<AuthBloc>(
-    () => AuthBloc(userSignUp: serviceLocator(), userSignIn: serviceLocator()),
+    () => AuthBloc(
+      userSignUp: serviceLocator(),
+      userSignIn: serviceLocator(),
+      getCurrentUser: serviceLocator(),
+      appUserCubit: serviceLocator(),
+    ),
   );
 }
