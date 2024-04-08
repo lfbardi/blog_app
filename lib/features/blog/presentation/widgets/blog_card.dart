f39c3c88d57a65e814b8d2b5a307f5d1374b4d9d
Luis Felipe Bardi <luisfbardi@gmail.com>
2024-04-08 14:33:10 -0300

    added gesture detector

--- lib/features/blog/presentation/widgets/blog_card.dart
+++ lib/features/blog/presentation/widgets/blog_card.dart
@@ -1,5 +1,6 @@
 import 'package:blog_app/core/utils/calculate_reading_time.dart';
 import 'package:blog_app/features/blog/domain/entities/blog.dart';
+import 'package:blog_app/features/blog/presentation/pages/blog_view_page.dart';
 import 'package:flutter/material.dart';
 
 class BlogCard extends StatelessWidget {
@@ -14,40 +15,45 @@ class BlogCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 200,
-      margin: const EdgeInsets.all(16).copyWith(bottom: 4),
-      padding: const EdgeInsets.all(16),
-      decoration: BoxDecoration(
-        color: color,
-        borderRadius: BorderRadius.circular(10),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          SingleChildScrollView(
-            scrollDirection: Axis.horizontal,
-            child: Row(
-              children: blog.topics
-                  .map(
-                    (e) => Padding(
-                      padding: const EdgeInsets.all(5),
-                      child: Chip(
-                        label: Text(e),
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(context, BlogViewerPage.route(blog));
+      },
+      child: Container(
+        height: 200,
+        margin: const EdgeInsets.all(16).copyWith(bottom: 4),
+        padding: const EdgeInsets.all(16),
+        decoration: BoxDecoration(
+          color: color,
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: blog.topics
+                    .map(
+                      (e) => Padding(
+                        padding: const EdgeInsets.all(5),
+                        child: Chip(
+                          label: Text(e),
+                        ),
                       ),
-                    ),
-                  )
-                  .toList(),
+                    )
+                    .toList(),
+              ),
             ),
-          ),
-          Text(
-            blog.title,
-            style: const TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
-          ),
-          const Spacer(),
-          Text('${calculateReadingTime(blog.content)} min')
-        ],
+            Text(
+              blog.title,
+              style: const TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+            ),
+            const Spacer(),
+            Text('${calculateReadingTime(blog.content)} min')
+          ],
+        ),
       ),
     );
   }
