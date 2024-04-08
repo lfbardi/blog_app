e1900dc06ec603345e5398d1a8c5d776fd2d8781
Luis Felipe Bardi <luisfbardi@gmail.com>
2024-04-08 13:38:09 -0300

    added get all blogs event and usecase

--- lib/features/blog/presentation/bloc/blog_bloc.dart
+++ lib/features/blog/presentation/bloc/blog_bloc.dart
@@ -1,20 +1,28 @@
 import 'dart:io';
 
+import 'package:blog_app/features/blog/domain/usecases/get_all_blogs.dart';
 import 'package:blog_app/features/blog/domain/usecases/upload_blog.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../../../core/usecase/usecase.dart';
+import '../../domain/entities/blog.dart';
 
 part 'blog_event.dart';
 part 'blog_state.dart';
 
 class BlogBloc extends Bloc<BlogEvent, BlogState> {
   final UploadBlog _uploadBlog;
+  final GetAllBlogs _getAllBlogs;
   BlogBloc({
     required UploadBlog uploadBlog,
+    required GetAllBlogs getAllBlogs,
   })  : _uploadBlog = uploadBlog,
+        _getAllBlogs = getAllBlogs,
         super(BlogInitial()) {
     on<BlogEvent>((event, emit) => emit(BlogLoading()));
     on<BlogUploadEvent>(_onBlogUpload);
+    on<GetAllBlogsEvent>(_onGetAllBlogs);
   }
 
   void _onBlogUpload(BlogUploadEvent event, Emitter<BlogState> emit) async {
@@ -30,7 +38,16 @@ class BlogBloc extends Bloc<BlogEvent, BlogState> {
 
     response.fold(
       (failure) => emit(BlogFailure(failure.message)),
-      (blog) => emit(BlogSuccess()),
+      (blog) => emit(BlogUploadSuccess()),
+    );
+  }
+
+  void _onGetAllBlogs(GetAllBlogsEvent event, Emitter<BlogState> emit) async {
+    final response = await _getAllBlogs(NoParams());
+
+    response.fold(
+      (failure) => emit(BlogFailure(failure.message)),
+      (blogs) => emit(BlogListSuccess(blogs)),
     );
   }
 }
