45a13d36c45d5ca4fba972725b763a583200f6ae
Luis Felipe Bardi <luisfbardi@gmail.com>
2024-04-08 15:23:47 -0300

    connection checker

---
+++ lib/core/network/connection_checker.dart
@@ -0,0 +1,13 @@
+import 'package:internet_connection_checker_plus/internet_connection_checker_plus.dart';
+
+abstract interface class ConnectionChecker {
+  Future<bool> get isConnected;
+}
+
+class ConnectionCheckerImpl implements ConnectionChecker {
+  final InternetConnection connection;
+  ConnectionCheckerImpl(this.connection);
+
+  @override
+  Future<bool> get isConnected async => await connection.hasInternetAccess;
+}
--- lib/features/auth/data/repository/auth_repository_impl.dart
+++ lib/features/auth/data/repository/auth_repository_impl.dart
@@ -1,14 +1,17 @@
 import 'package:blog_app/core/common/entities/user.dart';
 import 'package:blog_app/core/error/exceptions.dart';
 import 'package:blog_app/core/error/failures.dart';
+import 'package:blog_app/core/network/connection_checker.dart';
 import 'package:blog_app/features/auth/data/datasource/auth_remote_data_source.dart';
+import 'package:blog_app/features/auth/data/models/user_model.dart';
 import 'package:blog_app/features/auth/domain/repository/auth_repository.dart';
 import 'package:fpdart/fpdart.dart';
 import 'package:supabase_flutter/supabase_flutter.dart' as sb;
 
 class AuthRepositoryImpl implements AuthRepository {
   final AuthRemoteDataSource authRemoteDataSource;
-  const AuthRepositoryImpl(this.authRemoteDataSource);
+  final ConnectionChecker connectionChecker;
+  const AuthRepositoryImpl(this.authRemoteDataSource, this.connectionChecker);
 
   @override
   Future<Either<Failure, User>> signInWithEmailAndPassword({
@@ -40,6 +43,9 @@ class AuthRepositoryImpl implements AuthRepository {
 
   Future<Either<Failure, User>> _getUser(Future<User> Function() fn) async {
     try {
+      if (!await connectionChecker.isConnected) {
+        return left(Failure('No internet connection.'));
+      }
       final user = await fn();
       return right(user);
     } on sb.AuthException catch (e) {
@@ -52,6 +58,21 @@ class AuthRepositoryImpl implements AuthRepository {
   @override
   Future<Either<Failure, User>> getCurrentUser() async {
     try {
+      if (!await connectionChecker.isConnected) {
+        final session = authRemoteDataSource.currentUserSession;
+
+        if (session == null) {
+          return left(Failure('User is not logged in.'));
+        }
+
+        return right(
+          UserModel(
+            id: session.user.id,
+            email: session.user.email ?? '',
+            name: session.user.userMetadata!['name'],
+          ),
+        );
+      }
       final user = await authRemoteDataSource.getCurrentUserData();
       if (user == null) {
         return left(Failure('User is not logged in.'));
