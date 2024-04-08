11ba8f65791d593c24bc342b4400c0524e498b6b
Luis Felipe Bardi <luisfbardi@gmail.com>
2024-04-08 13:39:18 -0300

    init get all blogs usecase

--- lib/init_dependencies.dart
+++ lib/init_dependencies.dart
@@ -5,6 +5,7 @@ import 'package:blog_app/features/auth/domain/usecases/user_sign_in.dart';
 import 'package:blog_app/features/blog/data/datasources/blog_remote_data_source.dart';
 import 'package:blog_app/features/blog/data/repositories/blog_repositoy_impl.dart';
 import 'package:blog_app/features/blog/domain/repositories/blog_repository_dart.dart';
+import 'package:blog_app/features/blog/domain/usecases/get_all_blogs.dart';
 import 'package:blog_app/features/blog/domain/usecases/upload_blog.dart';
 import 'package:blog_app/features/blog/presentation/bloc/blog_bloc.dart';
 import 'package:get_it/get_it.dart';
@@ -70,7 +71,13 @@ void _initBlog() {
     ..registerLazySingleton<UploadBlog>(
       () => UploadBlog(serviceLocator()),
     )
+    ..registerLazySingleton<GetAllBlogs>(
+      () => GetAllBlogs(serviceLocator()),
+    )
     ..registerLazySingleton(
-      () => BlogBloc(uploadBlog: serviceLocator()),
+      () => BlogBloc(
+        uploadBlog: serviceLocator(),
+        getAllBlogs: serviceLocator(),
+      ),
     );
 }
