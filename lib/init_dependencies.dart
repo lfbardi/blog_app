3d4cf55df63290ad730c0ff8fab4f81037552632
Luis Felipe Bardi <luisfbardi@gmail.com>
2024-04-08 10:49:07 -0300

    init dependencies for blog bloc

--- lib/init_dependencies.dart
+++ lib/init_dependencies.dart
@@ -2,6 +2,11 @@ import 'package:blog_app/core/common/cubits/app_user/app_user_cubit.dart';
 import 'package:blog_app/core/secrets/app_secrets.dart';
 import 'package:blog_app/features/auth/domain/repository/auth_repository.dart';
 import 'package:blog_app/features/auth/domain/usecases/user_sign_in.dart';
+import 'package:blog_app/features/blog/data/datasources/blog_remote_data_source.dart';
+import 'package:blog_app/features/blog/data/repositories/blog_repositoy_impl.dart';
+import 'package:blog_app/features/blog/domain/repositories/blog_repository_dart.dart';
+import 'package:blog_app/features/blog/domain/usecases/upload_blog.dart';
+import 'package:blog_app/features/blog/presentation/bloc/blog_bloc.dart';
 import 'package:get_it/get_it.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
@@ -15,6 +20,7 @@ final serviceLocator = GetIt.instance;
 
 Future<void> initDependencies() async {
   _initAuth();
+  _initBlog();
   final supabase = await Supabase.initialize(
     url: AppSecrets.supabaseURL,
     anonKey: AppSecrets.supabaseApiKey,
@@ -27,28 +33,44 @@ Future<void> initDependencies() async {
 }
 
 void _initAuth() {
-  serviceLocator.registerLazySingleton<AuthRemoteDataSource>(
-    () => AuthRemoteDataSourceImpl(serviceLocator()),
-  );
-  serviceLocator.registerLazySingleton<AuthRepository>(
-    () => AuthRepositoryImpl(serviceLocator()),
-  );
-  serviceLocator.registerLazySingleton<UserSignUp>(
-    () => UserSignUp(serviceLocator()),
-  );
-  serviceLocator.registerLazySingleton<UserSignIn>(
-    () => UserSignIn(serviceLocator()),
-  );
-  serviceLocator.registerLazySingleton<GetCurrentUser>(
-    () => GetCurrentUser(serviceLocator()),
-  );
+  serviceLocator
+    ..registerLazySingleton<AuthRemoteDataSource>(
+      () => AuthRemoteDataSourceImpl(serviceLocator()),
+    )
+    ..registerLazySingleton<AuthRepository>(
+      () => AuthRepositoryImpl(serviceLocator()),
+    )
+    ..registerLazySingleton<UserSignUp>(
+      () => UserSignUp(serviceLocator()),
+    )
+    ..registerLazySingleton<UserSignIn>(
+      () => UserSignIn(serviceLocator()),
+    )
+    ..registerLazySingleton<GetCurrentUser>(
+      () => GetCurrentUser(serviceLocator()),
+    )
+    ..registerLazySingleton<AuthBloc>(
+      () => AuthBloc(
+        userSignUp: serviceLocator(),
+        userSignIn: serviceLocator(),
+        getCurrentUser: serviceLocator(),
+        appUserCubit: serviceLocator(),
+      ),
+    );
+}
 
-  serviceLocator.registerLazySingleton<AuthBloc>(
-    () => AuthBloc(
-      userSignUp: serviceLocator(),
-      userSignIn: serviceLocator(),
-      getCurrentUser: serviceLocator(),
-      appUserCubit: serviceLocator(),
-    ),
-  );
+void _initBlog() {
+  serviceLocator
+    ..registerLazySingleton<BlogRemoteDataSource>(
+      () => BlogRemoteDataSourceImpl(serviceLocator()),
+    )
+    ..registerLazySingleton<BlogRepository>(
+      () => BlogRepositoryImpl(serviceLocator()),
+    )
+    ..registerLazySingleton<UploadBlog>(
+      () => UploadBlog(serviceLocator()),
+    )
+    ..registerLazySingleton(
+      () => BlogBloc(uploadBlog: serviceLocator()),
+    );
 }
